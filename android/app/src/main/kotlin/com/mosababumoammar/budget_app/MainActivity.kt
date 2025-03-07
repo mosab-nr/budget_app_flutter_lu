6bd2d16a86da66b163b1368b58b4daefa1171447
Mosab N. R. AbuMoammar <mosab.abumoammar@gmail.com>
2025-03-07 15:51:11 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/mosababumoammar/budget_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mosababumoammar.budget_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/mobile/login_mobile_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../components.dart';
+
+class LoginViewMobile extends HookConsumerWidget {
+  const LoginViewMobile({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final viewModelProvider = ref.watch(viewModel);
+    final double deviceHeight = MediaQuery.of(context).size.height;
+    //  final double categoryWidth = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: deviceHeight / 5.5),
+              Image.asset("assets/logo.png", fit: BoxFit.contain, width: 210.0),
+              SizedBox(height: 30.0),
+              EmailAndPasswordFields(),
+              SizedBox(height: 30.0),
+              //Register
+              RegisterAndLogin(),
+              SizedBox(height: 30),
+              //Google signIn
+              GoogleSignInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
